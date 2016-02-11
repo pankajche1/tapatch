06e2d9bf198e62da6fc83764fc57a174a8293834
Pankaj Kumar <pankajche1@gmail.com>
2016-02-11 21:50:41 +0530

    contact us form default country India is put on select list

--- app/controllers/contact-us-controller.js
+++ app/controllers/contact-us-controller.js
@@ -10,6 +10,18 @@ module.exports=['$rootScope','$scope','Countries', function($rootScope,$scope, C
 				$scope.countries=Countries.query({},function(){
 				//console.log('success');
 				$scope.isCountriesLoading=false;
+				/*
+				angular.forEach(countries, function(value, key) {
+					  this.push(key + ': ' + value);
+				});
+				*/
+				for(i=0;i<$scope.countries.length;i++){
+					if($scope.countries[i].code=='IN'){
+						$scope.country=$scope.countries[i];
+						break;
+					}
+
+				}//for
 			
 				},
 				function(){
