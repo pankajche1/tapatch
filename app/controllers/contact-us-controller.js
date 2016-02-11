7aaed6164c96d528e930b00e23beb2d88cedea5b
Pankaj Kumar <pankajche1@gmail.com>
2016-02-11 22:56:45 +0530

    user date capture facility introduced in contact us form

--- app/controllers/contact-us-controller.js
+++ app/controllers/contact-us-controller.js
@@ -2,36 +2,49 @@
 // the service is Countries:
 module.exports=['$rootScope','$scope','Countries', function($rootScope,$scope, Countries){
 		//$scope.projects = [ {'name':'ayuroma'}, {'name':'mediatech'}];// projects list
-			$rootScope.$emit('ContactUsActive', 'Pankaj');
-			// the following {} is not making any effect
-			$scope.isCountriesLoading=false;
-			function fetchCountries(){
-				$scope.isCountriesLoading=true;
-				$scope.countries=Countries.query({},function(){
+		var country =  {};
+		$rootScope.$emit('ContactUsActive', 'Pankaj');
+		// the following {} is not making any effect
+		$scope.isCountriesLoading=false;
+		$scope.user={};
+		$scope.reset=function(){
+			$scope.user={'name':'','email':'','state':'','city':'','message':''};
+			$scope.user.country=country;
+
+		};//reset
+		$scope.send=function(user){
+			//$scope.user=user;
+
+		};//send
+
+		function fetchCountries(){
+			$scope.isCountriesLoading=true;
+			$scope.countries=Countries.query({},function(){
 				//console.log('success');
 				$scope.isCountriesLoading=false;
 				/*
-				angular.forEach(countries, function(value, key) {
-					  this.push(key + ': ' + value);
-				});
-				*/
+				 angular.forEach(countries, function(value, key) {
+				 this.push(key + ': ' + value);
+			});
+			*/
 				for(i=0;i<$scope.countries.length;i++){
 					if($scope.countries[i].code=='IN'){
-						$scope.country=$scope.countries[i];
+						country=$scope.countries[i];
+						$scope.user.country=$scope.countries[i];
 						break;
 					}
 
 				}//for
-			
-				},
-				function(){
+
+			},
+			function(){
 				$scope.isCountriesLoading=false;
-					//console.log('error');
-				});
+				//console.log('error');
+			});
 
 
-			}
-			fetchCountries();
+		}
+		fetchCountries();
 
 		/*	
 		$scope.countries=Countries.get({},function(){
