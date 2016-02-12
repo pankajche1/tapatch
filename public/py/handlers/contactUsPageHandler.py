53547157f809b17aea1a35293fd5fff1f8bfc8c8
Pankaj Kumar <pankajche1@gmail.com>
2016-02-12 13:53:34 +0530

    send mail to user facility introduced for contact us section

--- public/py/handlers/contactUsPageHandler.py
+++ public/py/handlers/contactUsPageHandler.py
@@ -3,6 +3,7 @@ import webapp2
 import jinja2
 import json
 from google.appengine.api import users
+from google.appengine.api import mail
 
 loader = jinja2.FileSystemLoader( \
                     os.path.join(os.path.dirname(__file__),'templates'))
@@ -26,11 +27,27 @@ class ContactUsPageHandler(webapp2.RequestHandler):
 
     def post(self):
         uName = self.request.get('name')
-        print 'uName:'+uName
+        #print 'uName:'+uName
         body =  json.loads(self.request.body)
         uName = body['name']
-        uName = 'from server'+uName
-        obj = { 'name': uName }
+        #uName = 'from server'+uName
+        emailAddress='pankajche1@gmail.com'
+        message='every thing is ok. mail sent'
+
+        if not mail.is_email_valid(emailAddress):
+            # prompt user to enter a valid address
+            message='mail address not valid'
+
+        else:
+            sender_address = "Taptach Support<support@tapach.com>"
+            subject = "Thanks for message"
+            body='''we have received your mail'''
+            if uName == u'yes send my mail':
+                mail.send_mail(sender_address, emailAddress, subject, body)
+                message += '... and mail sent by admin'
+            else:
+                message += '... but mail not sent by admin'
+        obj = { 'name': message, 'error':'no' }
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json.dumps(obj))
 
